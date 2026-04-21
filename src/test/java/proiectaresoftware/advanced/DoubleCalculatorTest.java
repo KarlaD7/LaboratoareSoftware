db9d4fbd07d8cf3e58faf89442534e208a0a0248
KarlaD7 <karlaiulia.durdun@ulbsibiu.ro>
2026-04-21 09:05:02 +0300

    7.6.1 & 7.6.2

--- src/test/java/proiectaresoftware/advanced/DoubleCalculatorTest.java
+++ src/test/java/proiectaresoftware/advanced/DoubleCalculatorTest.java
@@ -1,15 +1,119 @@
-package proiectaresoftware.advanced;
+/*package proiectaresoftware.advanced;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DoubleCalculatorTest {
-   @Test
+
+    @Test
     void addTest(){
-       double d=0.1;
-       assertTrue(d>0);
-   }
+        double d=0.1;
+        assertTrue(d>0);
+    }
 
+    private DoubleCalculator calculator;
+    private final double DELTA = 0.0001;
 
-}
+    @BeforeEach
+    public void setUp() {
+        calculator = new DoubleCalculator(0.0);
+    }
+
+    @Test
+    public void testAddPositives() {
+        // Arrange
+        calculator.init();
+        // Act
+        calculator.add(10.5).add(5.2);
+        // Assert
+        Assertions.assertEquals(15.7, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testAddNegatives() {
+        // Arrange
+        calculator.init();
+        // Act
+        calculator.add(-10.5).add(-5.2);
+        // Assert
+        Assertions.assertEquals(-15.7, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testSubtractPositives() {
+        // Arrange
+        calculator.init();
+        calculator.add(10.5); // Despărțit pe rând nou
+        // Act
+        calculator.subtract(5.2);
+        // Assert
+        Assertions.assertEquals(5.3, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testSubtractNegatives() {
+        // Arrange
+        calculator.init();
+        calculator.add(-10.5); // Despărțit pe rând nou
+        // Act
+        calculator.subtract(-5.2);
+        // Assert
+        Assertions.assertEquals(-5.3, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testMultiplyPositives() {
+        // Arrange
+        calculator.init();
+        calculator.add(10.0); // Despărțit pe rând nou
+        // Act
+        calculator.multiply(5.5);
+        // Assert
+        Assertions.assertEquals(55.0, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testMultiplyNegatives() {
+        // Arrange
+        calculator.init();
+        calculator.add(-10.0);
+        // Act
+        calculator.multiply(-5.5);
+        // Assert
+        Assertions.assertEquals(55.0, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testDividePositives() {
+        //arrange
+        calculator.init();
+        calculator.add(10.0);
+        //act
+        calculator.divide(2.0);
+        //assert
+        Assertions.assertEquals(5.0, (double) calculator.result(), DELTA);
+    }
+
+    @Test
+    public void testDivideNegatives() {
+        //arrange
+        calculator.init();
+        calculator.add(-10.0);
+        //act
+        calculator.divide(-2.0);
+        //assert
+        Assertions.assertEquals(5.0, (double) calculator.result(), DELTA);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (calculator != null) {
+            calculator.clear();
+            calculator = null;
+        }
+    }
+}*/
